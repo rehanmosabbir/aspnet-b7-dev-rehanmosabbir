90ca5143021a52053d906b0d8c93e97484e38a05
rehanmosabbir <rehanmosabbir@gmail.com>
2022-06-29 12:11:09 +0600

    add JsonFormatter class

---
+++ src/Assignment1/JsonSerializer/JsonFormatter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonSerializer
+{
+    public class JsonFormatter
+    {
+        public static string Convert(object item)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            IEnumerable<PropertyInfo> properties = item.GetType().GetProperties();
+            if (!item.GetType().IsPrimitive && item.GetType() != typeof(string) && item.GetType() != typeof(DateTime))
+            {
+                bool isArray = typeof(IEnumerable).IsAssignableFrom(item.GetType()) ? true : false;
+                if (isArray)
+                    stringBuilder.Append($"[");
+                else
+                    stringBuilder.Append($"{{");
+
+                if (!isArray)
+                    foreach (var property in properties)
+                        if (property.PropertyType.IsPrimitive || property.PropertyType == typeof(string))
+                            stringBuilder.Append($"\"{property.Name}\": \"{property.GetValue(item)}\", ");
+                        else
+                            stringBuilder.Append($"\"{property.Name}\": {Convert(property.GetValue(item))}, ");
+                else
+                    foreach (var i in item as IEnumerable)
+                        if (i.GetType().IsPrimitive || i.GetType() == typeof(string))
+                            stringBuilder.Append($"\"{i}\", ");
+                        else
+                            stringBuilder.Append($"{Convert(i)}, ");
+
+                stringBuilder.Remove(stringBuilder.ToString().Length - 2, 2);
+
+                if (isArray)
+                    stringBuilder.Append($"]");
+                else
+                    stringBuilder.Append($"}}");
+            }
+            else
+                stringBuilder.Append($"\"{item.ToString()}\"");
+
+            return stringBuilder.ToString();
+        }
+    }
+}
--- src/Assignment1/JsonSerializer/Program.cs
+++ src/Assignment1/JsonSerializer/Program.cs
@@ -1,1 +1,48 @@
-﻿
+﻿using JsonSerializer;
+
+Course course = new Course();
+course.Title = "Asp.net";
+course.Fees = 30000;
+course.Teacher = new Instructor()
+{
+    Name = "Jalaluddin",
+    Email = "jalaluddin@gmail.com",
+    PresentAddress = new Address()
+    {
+        City = "Dhaka",
+        Country = "Bangladesh",
+        Street = "37/A,Green Road"
+    },
+    PermanentAddress = new Address()
+    {
+        City = "Dhaka",
+        Country = "Bangladesh",
+        Street = "37/A, Green Road"
+    },
+    PhoneNumbers = new List<Phone> {
+        new Phone(){Number="1749291863", CountryCode="+880"},
+        new Phone(){Number="1835853215", CountryCode="+880"}
+    }
+};
+
+course.Topics = new List<Topic>()
+{
+    new Topic()
+    {
+        Title = "Getting Ready",
+        Description="Get to know each other, Purpose of learning, Proper way of learning",
+        Sessions = new List<Session>()
+        {
+            new Session(){DurationInHour=1,LearningObjective="Get to know each other"},
+            new Session(){DurationInHour=1,LearningObjective="Purpose of learning, Proper way of learning"},
+        }
+    }
+};
+
+course.Tests = new List<AdmissionTest>()
+{
+    new AdmissionTest(){StartDateTime=new DateTime(2022,1,21,10,0,0), EndDateTime=new DateTime(2022,1,21,11,0,0),TestFees=500},
+};
+
+string json = JsonFormatter.Convert(course);
+Console.WriteLine(json);
