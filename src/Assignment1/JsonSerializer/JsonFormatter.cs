e5ab7277dc23da67883779c3e284aa688799e4f3
rehanmosabbir <rehanmosabbir@gmail.com>
2022-06-30 23:37:23 +0600

    Modify JsonFormatter.cs

--- src/Assignment1/JsonSerializer/JsonFormatter.cs
+++ src/Assignment1/JsonSerializer/JsonFormatter.cs
@@ -23,17 +23,28 @@ namespace JsonSerializer
                     sb.Append($"{{");
 
                 if (!isArray)
+                {
                     foreach (var property in props)
+                    {
                         if (property.PropertyType.IsPrimitive || property.PropertyType == typeof(string))
                             sb.Append($"\"{property.Name}\": \"{property.GetValue(item)}\", ");
                         else
                             sb.Append($"\"{property.Name}\": {Convert(property.GetValue(item))}, ");
+                    }         
+                }
+                    
                 else
+                {
                     foreach (var i in item as IEnumerable)
+                    {
                         if (i.GetType().IsPrimitive || i.GetType() == typeof(string))
                             sb.Append($"\"{i}\", ");
                         else
                             sb.Append($"{Convert(i)}, ");
+                    }
+                        
+                }
+                    
 
                 sb.Remove(sb.ToString().Length - 2, 2);
 
@@ -43,7 +54,10 @@ namespace JsonSerializer
                     sb.Append($"}}");
             }
             else
+            {
                 sb.Append($"\"{item.ToString()}\"");
+            }
+               
 
             return sb.ToString();
         }
