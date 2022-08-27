9eedd8f02968ffbc7a9cf5cfa7be96143110393c
rehanmosabbir <rehanmosabbir@gmail.com>
2022-08-27 14:35:19 +0600

    solve Elephant_617A

---
+++ src/Assignment2/Elephant_617A/Program.cs
@@ -0,0 +1,23 @@
+﻿
+string inputStr = Console.ReadLine();
+int input = int.Parse(inputStr);
+int move;
+
+if (input > 5 && input % 5 != 0)
+{
+    decimal value = input / 5;
+    int remainder = (int)Math.Floor(value);
+    move = remainder + 1;
+}
+else if (input > 5 && input % 5 == 0)
+{
+    decimal value = input / 5;
+    int remainder = (int)Math.Floor(value);
+    move = remainder;
+}
+else
+{
+    move = 1;
+}
+
+Console.WriteLine(move);
