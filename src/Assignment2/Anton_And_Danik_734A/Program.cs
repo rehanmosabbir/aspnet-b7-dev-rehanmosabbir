e69d1ed0bfdec54bb102c47da5e182b503e88e55
rehanmosabbir <rehanmosabbir@gmail.com>
2022-08-27 14:52:05 +0600

    solve Anton_And_Danik_734A

---
+++ src/Assignment2/Anton_And_Danik_734A/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+int noOfGames = int.Parse(Console.ReadLine());
+
+string status = Console.ReadLine();
+int countAnton = 0;
+int countDanik = 0;
+
+for (int i = 0; i < status.Length; i++)
+{
+    if (status[i] == 'A')
+    {
+        countAnton++;
+    }
+    else
+    {
+        countDanik++;
+    }
+}
+
+if (countAnton == countDanik)
+{
+    Console.WriteLine("Friendship");
+}
+else if (countAnton > countDanik)
+{
+    Console.WriteLine("Anton");
+}
+else
+{
+    Console.WriteLine("Danik");
+}
