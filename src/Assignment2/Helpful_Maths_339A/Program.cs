120914c9c08a7ad0a86cde9e2eb1a6b688a08f09
rehanmosabbir <rehanmosabbir@gmail.com>
2022-08-27 12:55:28 +0600

    solve Helpful_Maths_339A

---
+++ src/Assignment2/Helpful_Maths_339A/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+string s = Console.ReadLine();
+string[] numStr = s.Split('+');
+int[] numbers = Array.ConvertAll(numStr, int.Parse);
+
+Array.Sort(numbers);
+
+string[] newStrArr = Array.ConvertAll(numbers, ele => ele.ToString());
+string str = string.Join("+", newStrArr);
+Console.WriteLine(str);
