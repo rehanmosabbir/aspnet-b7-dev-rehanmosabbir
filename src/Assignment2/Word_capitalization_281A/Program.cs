bbc981e9641b4a59548c5f3ba7a910af7330d9a7
rehanmosabbir <rehanmosabbir@gmail.com>
2022-08-27 13:01:57 +0600

    solve Word_capitalization_281A

---
+++ src/Assignment2/Word_capitalization_281A/Program.cs
@@ -0,0 +1,7 @@
+﻿
+string line = Console.ReadLine();
+string splitedString = line.Substring(1);
+
+char firstLetter = line[0];
+char firstLetterUpper = Char.ToUpper(firstLetter);
+Console.WriteLine(firstLetterUpper + splitedString);
