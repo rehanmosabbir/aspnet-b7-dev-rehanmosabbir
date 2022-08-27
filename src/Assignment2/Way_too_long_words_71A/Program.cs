22f3ebb5b77f12a54ac6a7181ec8143afb8ceed0
rehanmosabbir <rehanmosabbir@gmail.com>
2022-08-27 09:32:57 +0600

    solve Way_too_long_words_71A

---
+++ src/Assignment2/Way_too_long_words_71A/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+String noOfWordsInStr = Console.ReadLine();
+int noOfWords = int.Parse(noOfWordsInStr);
+
+String[] words = new String[noOfWords];
+
+for (int i = 0; i < noOfWords; i++)
+{
+    words[i] = Console.ReadLine();
+}
+
+foreach (String word in words)
+{
+    if (word.Length <= 10)
+    {
+        Console.WriteLine(word);
+    }
+    else
+    {
+        char firstChar = word[0];
+        string middleWord = word.Substring(1, word.Length - 2);
+        int middleWordLength = middleWord.Length;
+        string middleWordLengthStr = middleWordLength.ToString();
+        char lastChar = word[word.Length - 1];
+        Console.WriteLine(firstChar + middleWordLengthStr + lastChar);
+    }
+}
