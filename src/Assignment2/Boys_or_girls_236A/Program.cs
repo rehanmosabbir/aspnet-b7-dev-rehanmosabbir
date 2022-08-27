f4c2d754d4269e9d8c7ce65734a1150c7f1f9529
rehanmosabbir <rehanmosabbir@gmail.com>
2022-08-27 13:04:43 +0600

    solve Boys_or_girls_236A

---
+++ src/Assignment2/Boys_or_girls_236A/Program.cs
@@ -0,0 +1,27 @@
+﻿
+string userName = Console.ReadLine();
+char[] charArr = userName.ToCharArray();
+string unique = "";
+
+for (int i = 0; i < charArr.Length; i++)
+{
+    if (unique.Contains(charArr[i]))
+    {
+        continue;
+    }
+    else
+    {
+        unique = unique + charArr[i];
+
+    }
+}
+
+
+if (unique.Length % 2 == 0)
+{
+    Console.WriteLine("CHAT WITH HER!");
+}
+else
+{
+    Console.WriteLine("IGNORE HIM!");
+}
