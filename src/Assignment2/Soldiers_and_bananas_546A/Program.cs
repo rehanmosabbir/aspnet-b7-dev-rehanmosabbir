c29586c21e03ea0438fdbeb55fd75f6f025e97fb
rehanmosabbir <rehanmosabbir@gmail.com>
2022-08-27 14:29:56 +0600

    solve Soldiers_and_bananas_546A

---
+++ src/Assignment2/Soldiers_and_bananas_546A/Program.cs
@@ -0,0 +1,18 @@
+﻿
+string[] str = Console.ReadLine().Split(' ');
+int k = int.Parse(str[0]);
+int n = int.Parse(str[1]);
+int w = int.Parse(str[2]);
+int price = 0;
+for (int i = 1; i <= w; i++)
+{
+    price = price + (i * k);
+}
+if (price < n || price == n)
+{
+    Console.WriteLine(0);
+}
+else
+{
+    Console.WriteLine(price - n);
+}
