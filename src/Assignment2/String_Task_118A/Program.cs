e3d62c1e60939de2a68fb84c039f92719735424a
rehanmosabbir <rehanmosabbir@gmail.com>
2022-08-27 12:57:50 +0600

    solve String_Task_118A

---
+++ src/Assignment2/String_Task_118A/Program.cs
@@ -0,0 +1,15 @@
+﻿
+string line = Console.ReadLine().ToLower();
+string str = "";
+for (int i = 0; i < line.Length; i++)
+{
+    if (line[i].Equals('a') || line[i].Equals('e') || line[i].Equals('i') || line[i].Equals('o') || line[i].Equals('u') || line[i].Equals('y'))
+    {
+        continue;
+    }
+    else
+    {
+        str += "." + line[i];
+    }
+}
+Console.WriteLine(str);
