6665d1c9d1eb0c4f590f91226bb340b2e00a7728
rehanmosabbir <rehanmosabbir@gmail.com>
2022-08-27 09:42:04 +0600

    solve TheatreSquare_1A

---
+++ src/Assignment2/TheatreSquare_1A/Program.cs
@@ -0,0 +1,22 @@
+﻿
+string[] line = Console.ReadLine().Split(' ');
+int n = int.Parse(line[0]);
+int m = int.Parse(line[1]);
+int a = int.Parse(line[2]);
+
+long x, y, sum;
+
+x = n / a;
+y = m / a;
+if (n % a != 0)
+{
+    x = x + 1;
+}
+
+if (m % a != 0)
+{
+    y = y + 1;
+}
+
+sum = x * y;
+Console.WriteLine(sum);
