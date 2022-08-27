e64e7526ea71c19ce9a6f5203c8bb1c830b41799
rehanmosabbir <rehanmosabbir@gmail.com>
2022-08-27 11:20:06 +0600

    solve Bit_plus_plus_282A

---
+++ src/Assignment2/Bit_plus_plus_282A/Program.cs
@@ -0,0 +1,17 @@
+﻿
+string numOfStatementStr = Console.ReadLine();
+int numOfStatement = int.Parse(numOfStatementStr);
+int x = 0;
+for (int i = 0; i < numOfStatement; i++)
+{
+    string statement = Console.ReadLine();
+    if (statement == "X++" || statement == "++X")
+    {
+        x = x + 1;
+    }
+    else if (statement == "X--" || statement == "--X")
+    {
+        x = x - 1;
+    }
+}
+Console.WriteLine(x);
