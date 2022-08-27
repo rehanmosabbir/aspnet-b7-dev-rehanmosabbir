f2f049e6661ab73a445d2c8bc43938f7cee4cf97
rehanmosabbir <rehanmosabbir@gmail.com>
2022-08-27 12:50:57 +0600

    solve Petya_and_strings_112A

---
+++ src/Assignment2/Petya_and_strings_112A/Program.cs
@@ -0,0 +1,16 @@
+﻿
+string firstLine = Console.ReadLine().ToLower();
+string secondLine = Console.ReadLine().ToLower();
+
+if (firstLine == secondLine)
+{
+    Console.WriteLine(0);
+}
+else if (firstLine.CompareTo(secondLine) < 0)
+{
+    Console.WriteLine(-1);
+}
+else if (firstLine.CompareTo(secondLine) > 0)
+{
+    Console.WriteLine(1);
+}
