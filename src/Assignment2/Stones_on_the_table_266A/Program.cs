92c20a742793b958537a5daeb5050de563ff3f10
rehanmosabbir <rehanmosabbir@gmail.com>
2022-08-27 13:21:29 +0600

    solve Stones_on_the_table_266A

---
+++ src/Assignment2/Stones_on_the_table_266A/Program.cs
@@ -0,0 +1,16 @@
+﻿
+string b;
+int a, c = 0;
+
+a = int.Parse(Console.ReadLine());
+b = Console.ReadLine();
+
+for (int i = 1; i < a; i++)
+{
+    if ((char)b[i - 1] == (char)b[i])
+    {
+        c++;
+    }
+}
+
+Console.WriteLine(c);
