998a2e2b923fc9b4d3a39b21eac31558ed9c6af7
rehanmosabbir <rehanmosabbir@gmail.com>
2022-08-27 09:20:45 +0600

    solve Watermelon_4A

---
+++ src/Assignment2/Watermelon_4A/Program.cs
@@ -0,0 +1,10 @@
+﻿
+string? line = Console.ReadLine();
+int w = int.Parse(line);
+
+if (w % 2 == 1)
+    Console.WriteLine("NO");
+else if (w == 2)
+    Console.WriteLine("NO");
+else
+    Console.WriteLine("YES");
