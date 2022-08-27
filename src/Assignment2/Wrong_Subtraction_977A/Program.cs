e3f2b7852d008ce29237f1678d59d65e0568c2dd
rehanmosabbir <rehanmosabbir@gmail.com>
2022-08-27 14:44:36 +0600

    solve Wrong_Subtraction_977A

---
+++ src/Assignment2/Wrong_Subtraction_977A/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+string[] str = Console.ReadLine().Split(' ');
+string substract = str[0];
+int substractNum = int.Parse(substract);
+int lastDigit = substractNum % 10;
+int times = int.Parse(str[1]);
+for (int i = 0; i < times; i++)
+{
+    if (lastDigit == 0)
+    {
+        substractNum = substractNum / 10;
+        lastDigit = substractNum % 10;
+
+    }
+    else
+    {
+
+        substractNum--;
+        lastDigit = substractNum % 10;
+    }
+}
+Console.WriteLine(substractNum);
