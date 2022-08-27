e1eeadfc541ee8632958c1bf971bbf0f8935ae0b
rehanmosabbir <rehanmosabbir@gmail.com>
2022-08-27 09:35:46 +0600

    solve Team_231A

---
+++ src/Assignment2/Team_231A/Program.cs
@@ -0,0 +1,27 @@
+﻿
+int noOfProblems = int.Parse(Console.ReadLine());
+int[] sum = new int[noOfProblems];
+int newSum = 0;
+
+for (int i = 0; i < noOfProblems; i++)
+{
+    string[] a = Console.ReadLine().Split(' ');
+
+    for (int j = 0; j < a.Length; j++)
+    {
+        if (int.Parse(a[j]) == 1)
+        {
+            sum[i]++;
+        }
+    }
+}
+
+for (int i = 0; i < sum.Length; i++)
+{
+    if (sum[i] >= 2)
+    {
+        newSum++;
+    }
+}
+
+Console.WriteLine(newSum);
