4b6a29735eba782b2df5000e83b8f1b986268bdf
rehanmosabbir <rehanmosabbir@gmail.com>
2022-08-27 14:47:02 +0600

    solve Nearly_Lucky_Number_110A

---
+++ src/Assignment2/Nearly_Lucky_Number_110A/Program.cs
@@ -0,0 +1,17 @@
+﻿
+string str = Console.ReadLine();
+
+int count = 0;
+
+for (int i = 0; i < str.Length; i++)
+{
+    if (str[i] == '4' || str[i] == '7')
+    {
+        count++;
+    }
+
+}
+if (count == 4 || count == 7)
+    Console.WriteLine("YES");
+else
+    Console.WriteLine("NO");
