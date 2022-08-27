4c2f6a1859d2702101b8f574380d90a623d0438e
rehanmosabbir <rehanmosabbir@gmail.com>
2022-08-27 14:37:20 +0600

    solve Word_59A

---
+++ src/Assignment2/Word_59A/Program.cs
@@ -0,0 +1,22 @@
+﻿String s = Console.ReadLine();
+int upperCount = 0;
+int lowerCount = 0;
+for (int i = 0; i < s.Length; i++)
+{
+    char c = s[i];
+    if (Char.IsUpper(c)) { upperCount++; }
+    if (Char.IsLower(c)) { lowerCount++; }
+}
+
+if (upperCount > lowerCount)
+{
+    Console.WriteLine(s.ToUpper());
+}
+else if (lowerCount > upperCount)
+{
+    Console.WriteLine(s.ToLower());
+}
+else
+{
+    Console.WriteLine(s.ToLower());
+}
