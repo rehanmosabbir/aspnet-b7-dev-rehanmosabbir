87a1ce5e3619591a04bde356ecb4970296767352
rehanmosabbir <rehanmosabbir@gmail.com>
2022-08-27 14:27:01 +0600

    solve Bear_and_big_brother_791A

---
+++ src/Assignment2/Bear_and_big_brother_791A/Program.cs
@@ -0,0 +1,28 @@
+﻿
+string[] weights = Console.ReadLine().Split(' ');
+
+int a = int.Parse(weights[0]);
+int b = int.Parse(weights[1]);
+
+int sum = 0;
+
+int i = 3;
+int j = 2;
+
+while (true)
+{
+    if (a * i > b * j)
+    {
+        sum++;
+        break;
+    }
+    else
+    {
+        sum++;
+        i = i * 3;
+        j = j * 2;
+        continue;
+    }
+}
+
+Console.WriteLine(sum);
