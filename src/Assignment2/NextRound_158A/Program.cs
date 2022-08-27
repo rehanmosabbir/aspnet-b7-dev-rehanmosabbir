f022a0d70249434d859f698abb2374b10ed42015
rehanmosabbir <rehanmosabbir@gmail.com>
2022-08-27 09:45:01 +0600

    solve NextRound_158A

---
+++ src/Assignment2/NextRound_158A/Program.cs
@@ -0,0 +1,17 @@
+﻿
+string[] arr = Console.ReadLine().Split(' ');
+int n = int.Parse(arr[0]);
+int k = int.Parse(arr[1]);
+string[] scores = Console.ReadLine().Split(' ');
+int x = int.Parse(scores[k - 1]);
+int sum = 0;
+
+for (int i = 0; i < scores.Length; i++)
+{
+    if (int.Parse(scores[i]) >= x && int.Parse(scores[i]) > 0)
+    {
+        sum++;
+    }
+}
+
+Console.WriteLine(sum);
