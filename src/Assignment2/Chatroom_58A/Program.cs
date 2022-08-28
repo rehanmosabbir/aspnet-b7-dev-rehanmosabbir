6bc387ca64d4e315fb6eb8efc49e36cddfd7f2df
rehanmosabbir <rehanmosabbir@gmail.com>
2022-08-28 10:29:47 +0600

    solve Chatroom_58A

---
+++ src/Assignment2/Chatroom_58A/Program.cs
@@ -0,0 +1,25 @@
+﻿
+string a, b = "hello";
+int j = 0, pas = 0;
+a = Console.ReadLine();
+for (int i = 0; i < a.Length; i++)
+{
+    if (a[i] == b[j])
+    {
+        j++;
+        pas++;
+
+        if (pas == 5)
+        {
+           break;
+        }
+    }
+}
+if (pas == 5)
+{
+  Console.WriteLine("YES");
+}
+else
+{
+  Console.WriteLine("NO");
+}
