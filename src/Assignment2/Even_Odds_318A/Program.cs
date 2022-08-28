9e2f76b30de842433d79516e1c54f71e5dadd991
rehanmosabbir <rehanmosabbir@gmail.com>
2022-08-28 12:10:55 +0600

    solve Even_Odds_318A

---
+++ src/Assignment2/Even_Odds_318A/Program.cs
@@ -0,0 +1,13 @@
+﻿
+long n, k;
+string[] str = Console.ReadLine().Split(' ');
+n = long.Parse(str[0]);
+k = long.Parse(str[1]);
+if (k <= (n + 1) / 2)
+{
+    Console.WriteLine(k * 2 - 1);  
+}
+else
+{
+    Console.WriteLine((k - (n + 1) / 2) * 2);
+}
