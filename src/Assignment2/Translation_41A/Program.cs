3c6e960cfc3f3710dfbbe437197754f164624509
rehanmosabbir <rehanmosabbir@gmail.com>
2022-08-28 10:22:36 +0600

    solve Translation_41A

---
+++ src/Assignment2/Translation_41A/Program.cs
@@ -0,0 +1,19 @@
+﻿
+string mainWord = Console.ReadLine();
+string reversedWord = Console.ReadLine();
+
+string newString = string.Empty;
+
+for (int i = reversedWord.Length - 1; i >= 0; i--)
+{
+    newString += reversedWord[i];
+}
+
+if (mainWord == newString)
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
+}
