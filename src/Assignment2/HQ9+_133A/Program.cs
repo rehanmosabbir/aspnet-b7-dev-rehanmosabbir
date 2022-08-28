ce4d70ea1b9bddc29d62555c4e993f2e3c0c84c1
rehanmosabbir <rehanmosabbir@gmail.com>
2022-08-28 12:03:45 +0600

    solve HQ9+_133A

---
+++ src/Assignment2/HQ9+_133A/Program.cs
@@ -0,0 +1,19 @@
+﻿string str;
+str = Console.ReadLine();
+int flag = 0;
+for (int i = 0; i < str.Length; i++)
+{
+	if (str[i] >= 33 && str[i] <= 126)
+	{
+		if (str[i] == 'H' || str[i] == 'Q' || str[i] == '9')
+			flag = 1;
+	}
+}
+if (flag != 0)
+{
+    Console.WriteLine("YES");
+}
+else
+{
+	Console.WriteLine("NO");
+}
