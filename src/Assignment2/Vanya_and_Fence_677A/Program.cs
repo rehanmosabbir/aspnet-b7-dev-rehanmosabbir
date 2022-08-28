65bc5d9527196d416357f9d9fb92354e2cf7e2ca
rehanmosabbir <rehanmosabbir@gmail.com>
2022-08-28 10:37:10 +0600

    solve Vanya_and_Fence_677A

---
+++ src/Assignment2/Vanya_and_Fence_677A/Program.cs
@@ -0,0 +1,45 @@
+﻿
+string line = Console.ReadLine();
+string line2 = Console.ReadLine();
+string[] splitedLine = line.Split(' ');
+string[] splitedLine2 = line2.Split(' ');
+int[] userHeight = new int[splitedLine2.Length];
+int[] userNumAndWallHeight = new int[splitedLine.Length];
+int[] newarray = new int[userHeight.Length];
+
+
+
+for (int i = 0; i < splitedLine2.Length; i++)
+{
+    userHeight[i] = int.Parse(splitedLine2[i]);
+}
+for (int i = 0; i < splitedLine.Length; i++)
+{
+    userNumAndWallHeight[i] = int.Parse(splitedLine[i]);
+}
+
+int userNum = userNumAndWallHeight[0];
+int wallHeight = userNumAndWallHeight[1];
+
+for (int i = 0; i < userNum; i++)
+{
+
+    if (((float)userHeight[i] / (float)wallHeight) <= 1)
+    {
+        newarray[i] = 1;
+    }
+    else
+    {
+        newarray[i] = 2;
+    }
+}
+
+
+
+int sum = 0;
+for (int i = 0; i < newarray.Length; i++)
+{
+    sum += newarray[i];
+}
+
+Console.WriteLine(sum);
