57523d008610b91e89fa86a812cf55ffd0d28298
rehanmosabbir <rehanmosabbir@gmail.com>
2022-08-28 12:18:24 +0600

    solve Ultra-Fast_Mathematician_61A

---
+++ src/Assignment2/Ultra-Fast_Mathematician_61A/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+string firstNum = Console.ReadLine();
+
+string secondNum = Console.ReadLine();
+
+string newStr = string.Empty;
+
+for (int i = 0; i < firstNum.Length; i++)
+{
+
+    if (firstNum[i] == secondNum[i])
+    {
+        newStr += "0";
+    }
+    else
+    {
+        newStr += "1";
+    }
+
+}
+
+Console.WriteLine(newStr);
