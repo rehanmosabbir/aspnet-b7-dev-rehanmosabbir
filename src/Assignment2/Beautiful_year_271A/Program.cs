fd6819e0ff3e097b2fdb141d34ec9c1607707f81
rehanmosabbir <rehanmosabbir@gmail.com>
2022-08-28 10:34:44 +0600

    solve Beautiful_year_271A

---
+++ src/Assignment2/Beautiful_year_271A/Program.cs
@@ -0,0 +1,16 @@
+﻿int y;
+y = int.Parse(Console.ReadLine());
+while (true)
+{
+    y += 1;
+    int a = y / 1000;
+    int b = y / 100 % 10;
+    int c = y / 10 % 10;
+    int d = y % 10;
+    if (a != b && a != c && a != d && b != c && b != d && c != d)
+    {
+        break;
+    }
+}
+Console.WriteLine(y);
+
