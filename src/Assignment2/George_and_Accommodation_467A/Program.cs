5d2790e35411fe61378d32949744e34e6fe51bc9
rehanmosabbir <rehanmosabbir@gmail.com>
2022-08-28 11:56:19 +0600

    solve George_and_Accommodation_467A

---
+++ src/Assignment2/George_and_Accommodation_467A/Program.cs
@@ -0,0 +1,21 @@
+﻿
+int noOfRooms = int.Parse(Console.ReadLine());
+int count = 0;
+
+for (int i = 0; i < noOfRooms; i++)
+{
+    string[] people = Console.ReadLine().Split(' ');
+    int alreadyLive = int.Parse(people[0]);
+    int capacity = int.Parse(people[1]);
+
+    if (alreadyLive + 2 <= capacity)
+    {
+        count++;
+    }
+    else
+    {
+        continue;
+    }
+}
+
+Console.WriteLine(count);
