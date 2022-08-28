96d7da8a8ae73e5f9721930583c070835d3081b7
rehanmosabbir <rehanmosabbir@gmail.com>
2022-08-28 11:59:11 +0600

    solve In_Search_of_an_Easy_Problem_1030A

---
+++ src/Assignment2/In_Search_of_an_Easy_Problem_1030A/Program.cs
@@ -0,0 +1,20 @@
+﻿
+int noOfPeople = int.Parse(Console.ReadLine());
+string result = string.Empty;
+
+string[] question = Console.ReadLine().Split(' ');
+for (int i = 0; i < question.Length; i++)
+{
+    if (int.Parse(question[i]) == 0)
+    {
+        result = "EASY";
+        continue;
+    }
+    else
+    {
+        result = "Hard";
+        break;
+    }
+}
+
+Console.WriteLine(result);
