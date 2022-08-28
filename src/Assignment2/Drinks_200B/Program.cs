cb80c85568a30bdb332215251eb5af94f91fa772
rehanmosabbir <rehanmosabbir@gmail.com>
2022-08-28 12:16:03 +0600

    solve Drinks_200B

---
+++ src/Assignment2/Drinks_200B/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+int n = int.Parse(Console.ReadLine());
+string[] p = Console.ReadLine().Split(' ');
+
+int sum = 0;
+
+for (int i = 0; i < p.Length; i++)
+{
+    sum += int.Parse(p[i]);
+}
+
+Console.WriteLine((decimal)sum / n);
