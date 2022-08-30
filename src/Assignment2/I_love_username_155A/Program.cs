6dbfcb0b109456829de3328dcaf10e4da8a6d4b4
rehanmosabbir <rehanmosabbir@gmail.com>
2022-08-30 10:23:11 +0600

    solve I_love_username_155A

---
+++ src/Assignment2/I_love_username_155A/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+int n;
+n = int.Parse(Console.ReadLine());
+int[] score = new int[n];
+string[] arr = Console.ReadLine().Split(' ');
+for (int i = 0; i < n; i++)
+{
+   score[i] = int.Parse(arr[i]);
+}
+int answer = 0;
+
+for (int i = 0; i < n; i++)
+{
+    if (i == 0) continue;
+    bool isMax = true, isMin = true;
+    for (int j = 0; j < i; j++)
+    {
+        if (score[j] <= score[i])
+        {
+            isMin = false;
+        }
+        if (score[j] >= score[i])
+        {
+            isMax = false;
+        }
+    }
+    if (isMin || isMax)
+    {
+        answer++;
+    }
+}
+
+Console.WriteLine(answer);
