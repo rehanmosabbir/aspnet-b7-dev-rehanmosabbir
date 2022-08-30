eb1d7c747862ca32d358fc2dc2b8e14b72937b83
rehanmosabbir <rehanmosabbir@gmail.com>
2022-08-30 10:25:49 +0600

    solve Mishka_and_Game_703A

---
+++ src/Assignment2/Mishka_and_Game_703A/Program.cs
@@ -0,0 +1,36 @@
+﻿
+int noOfRound = int.Parse(Console.ReadLine());
+
+int countMishka = 0;
+int countChris = 0;
+for (int i = 0; i < noOfRound; i++)
+{
+    string[] points = Console.ReadLine().Split(' ');
+    int pointOfMishka = int.Parse(points[0]);
+    int pointOfChris = int.Parse(points[1]);
+    if (pointOfMishka > pointOfChris)
+    {
+        countMishka++;
+    }
+    else if (pointOfChris > pointOfMishka)
+    {
+        countChris++;
+    }
+    else
+    {
+        continue;
+    }
+}
+
+if (countMishka > countChris)
+{
+    Console.WriteLine("Mishka");
+}
+else if (countChris > countMishka)
+{
+    Console.WriteLine("Chris");
+}
+else
+{
+    Console.WriteLine("Friendship is magic!^^");
+}
