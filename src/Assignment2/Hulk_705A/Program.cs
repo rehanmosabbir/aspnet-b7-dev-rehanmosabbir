25b975fb5ef19dc9e5c3edea302f5fb6879efa3b
rehanmosabbir <rehanmosabbir@gmail.com>
2022-08-30 09:43:39 +0600

    solve Hulk_705A

---
+++ src/Assignment2/Hulk_705A/Program.cs
@@ -0,0 +1,21 @@
+﻿
+int a = int.Parse(Console.ReadLine());
+for (int i = 0; i < a; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write("I hate ");
+    }
+    else
+    {
+        Console.Write("I love ");
+    }
+    if (i != a - 1)
+    {
+        Console.Write("that ");
+    }
+    else
+    {
+        Console.Write("it ");
+    }
+}
