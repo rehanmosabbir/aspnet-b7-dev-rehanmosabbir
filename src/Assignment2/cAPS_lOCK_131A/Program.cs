70087ab9133debbe7fd6d73771c743ef1ee5400b
rehanmosabbir <rehanmosabbir@gmail.com>
2022-08-30 10:06:16 +0600

    solve cAPS_lOCK_131A

---
+++ src/Assignment2/cAPS_lOCK_131A/Program.cs
@@ -0,0 +1,27 @@
+﻿
+string s;
+char u;
+bool c = true;
+s = Console.ReadLine();
+
+for (int i = 1; i < s.Length; i++)
+{
+    if (Char.IsLower(s[i]))
+    {
+        c = false;
+    }
+}
+
+if (c == true)
+{
+    for (int j = 0; j < s.Length; j++)
+    {
+        if (Char.IsLower(s[j]))
+            u = char.ToUpper(s[j]);
+        else
+            u = char.ToLower(s[j]);
+        Console.Write(u);
+    }
+}
+else
+    Console.WriteLine(s);
