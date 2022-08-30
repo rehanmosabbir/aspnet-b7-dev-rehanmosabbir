c59ea7d7dfcb237e703b66a752e2dd526f74cd09
rehanmosabbir <rehanmosabbir@gmail.com>
2022-08-30 10:09:10 +0600

    solve Divisibility_Problem_1328A

---
+++ src/Assignment2/Divisibility_Problem_1328A/Program.cs
@@ -0,0 +1,21 @@
+﻿
+int t = int.Parse(Console.ReadLine());
+for (int i = 0; i < t; i++)
+{
+    string[] p = Console.ReadLine().Split(' ');
+    int a = int.Parse(p[0]);
+    int b = int.Parse(p[1]);
+    int quotient = a / b;
+    int nextQuotient = quotient + 1;
+    int next_a = nextQuotient * b;
+    if (a % b == 0)
+    {
+        Console.WriteLine(0);
+    }
+    else
+    {
+        int answer = next_a - a;
+        Console.WriteLine(answer);
+    }
+
+}
