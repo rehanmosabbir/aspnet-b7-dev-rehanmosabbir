c6af53ca0df7ea751489bd3e26ac24e4b4be520b
rehanmosabbir <rehanmosabbir@gmail.com>
2022-08-31 23:05:40 +0600

    solve Anton_and_Polyhedrons_785A

---
+++ src/Assignment2/Anton_and_Polyhedrons_785A/Program.cs
@@ -0,0 +1,29 @@
+﻿
+int a, count = 0;
+a = int.Parse(Console.ReadLine());
+string[] arr = new string[a];
+for (int i = 0; i < a; i++)
+{
+    arr[i] = Console.ReadLine();
+    if (arr[i] == "Tetrahedron")
+    {
+        count += 4;
+    }
+    else if (arr[i] == "Cube")
+    {
+        count += 6;
+    }
+    else if (arr[i] == "Octahedron")
+    {
+        count += 8;
+    }
+    else if (arr[i] == "Dodecahedron")
+    {
+        count += 12;
+    }
+    else if (arr[i] == "Icosahedron")
+    {
+        count += 20;
+    }
+}
+Console.WriteLine(count);
