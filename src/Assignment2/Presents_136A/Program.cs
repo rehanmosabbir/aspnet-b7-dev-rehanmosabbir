e68fa8b45ef03971d5a808ea0352c1485f020ce7
rehanmosabbir <rehanmosabbir@gmail.com>
2022-08-31 23:41:07 +0600

    solve Presents_136A

---
+++ src/Assignment2/Presents_136A/Program.cs
@@ -0,0 +1,19 @@
+﻿int a;
+a = int.Parse(Console.ReadLine());
+int[] arr = new int[a];
+string[] str = Console.ReadLine().Split(' ');
+for (int i = 0; i < a; i++)
+{
+    arr[i] = int.Parse(str[i]);
+}
+for (int j = 1; j <= a; j++)
+{
+    for (int i = 0; i < a; i++)
+    {
+        if (arr[i] == j)
+        {
+            Console.Write(i+1);
+            Console.Write(" ");
+        }
+    }
+}
