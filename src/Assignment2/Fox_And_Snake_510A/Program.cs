d0eb702cd20b5e99f463c51380d99def03a68ee9
rehanmosabbir <rehanmosabbir@gmail.com>
2022-08-31 23:13:07 +0600

    solve Fox_And_Snake_510A

---
+++ src/Assignment2/Fox_And_Snake_510A/Program.cs
@@ -0,0 +1,26 @@
+﻿
+int a, b;
+string[] arr = Console.ReadLine().Split(' ');
+a = int.Parse(arr[0]);
+b = int.Parse(arr[1]);
+for (int i = 1; i <= a; i++)
+{
+    for (int j = 0; j < b; j++)
+    {
+        if (i % 4 == 0)
+        {
+            if (j == 0) Console.Write("#");
+            else Console.Write(".");
+        }
+        else if (i % 4 == 2)
+        {
+            if (j == b - 1) Console.Write("#");
+            else Console.Write(".");
+        }
+        else
+        {
+            Console.Write("#");
+        }
+    }
+    Console.WriteLine();
+}
