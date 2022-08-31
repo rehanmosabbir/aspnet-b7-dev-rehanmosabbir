2d8f80f88e5b1d51be1b15e05c3e8763b988bca3
rehanmosabbir <rehanmosabbir@gmail.com>
2022-08-31 09:46:34 +0600

    Football_43A

---
+++ src/Assignment2/Football_43A/Program.cs
@@ -0,0 +1,26 @@
+﻿
+int n, goal = 0;
+n = int.Parse(Console.ReadLine());
+
+string team, win = null;
+for (int i = n; i > 0; i--)
+{
+    if (goal != 0)
+    {
+        team = Console.ReadLine();
+        if (team == win)
+        {
+            goal += 1;
+        }
+        else
+        {
+            goal -= 1;
+        }
+    }
+    else
+    {
+        win = Console.ReadLine();
+        goal = 1;
+    }
+}
+Console.WriteLine(win);
