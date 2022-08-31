96083b3eedc7e68a47ef4eb4a79959d9133c5615
rehanmosabbir <rehanmosabbir@gmail.com>
2022-08-31 10:47:00 +0600

    solve Square_String_1619A

---
+++ src/Assignment2/Square_String_1619A/Program.cs
@@ -0,0 +1,22 @@
+﻿
+int n = int.Parse(Console.ReadLine());
+for(int i = n; i > 0; i--)
+{
+    string s = Console.ReadLine();
+    int count = 0;
+    int mid = s.Length / 2;
+    if (s.Length == 1) Console.WriteLine("NO");
+    else if(s.Length % 2 == 0)
+    {
+        for(int j = 0; j < mid; j++)
+        {
+            if (s[j] == s[mid + j])
+            {
+                count++;
+            }
+        }
+        if (count == mid) Console.WriteLine("YES");
+        else Console.WriteLine("NO");
+    }
+    else Console.WriteLine("NO");
+}
