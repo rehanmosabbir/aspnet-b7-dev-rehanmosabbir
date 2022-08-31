27909df0892e470ca7eebda1d90f1f778432d68f
rehanmosabbir <rehanmosabbir@gmail.com>
2022-08-31 23:23:12 +0600

    solve Game_With_Sticks_451A

---
+++ src/Assignment2/Game_With_Sticks_451A/Program.cs
@@ -0,0 +1,21 @@
+﻿
+int n, m,i;
+string[] str = Console.ReadLine().Split(' ');
+n = int.Parse(str[0]);
+m = int.Parse(str[1]);
+if (n > m)
+{
+    i = m;
+}
+else
+{
+    i = n;
+}
+if (i % 2 == 0)
+{
+    Console.WriteLine("Malvika");
+}
+else
+{
+    Console.WriteLine("Akshat");
+}
