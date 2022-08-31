e555f683007a5e1bd574e674ea7ba8bdcd6fe268
rehanmosabbir <rehanmosabbir@gmail.com>
2022-08-31 23:56:44 +0600

    solve Amusing_Joke_141A

---
+++ src/Assignment2/Amusing_Joke_141A/Program.cs
@@ -0,0 +1,19 @@
+﻿
+string s1, s2, s3;
+s1 = Console.ReadLine();
+s2 = Console.ReadLine();
+s3 = Console.ReadLine();
+string s4 = s1 + s2;
+
+
+string s5 = String.Concat(s3.OrderBy(c => c));
+string s6 = String.Concat(s4.OrderBy(c => c));
+
+if (s5 == s6)
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
+}
