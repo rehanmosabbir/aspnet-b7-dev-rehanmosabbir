d4661f59890b0045ab3ba37f1eb4afd38c29a7ba
rehanmosabbir <rehanmosabbir@gmail.com>
2022-08-31 23:18:56 +0600

    solve Police_Recruits_427A

---
+++ src/Assignment2/Police_Recruits_427A/Program.cs
@@ -0,0 +1,20 @@
+﻿int n, e;
+int p = 0, c = 0;
+n = int.Parse(Console.ReadLine());
+
+string[] str = Console.ReadLine().Split(' ');
+for (int i = 0; i < n; i++)
+{
+    e = int.Parse(str[i]);
+    if (e == -1)
+    {
+        if (p > 0)
+            p--;
+        else
+            c++;
+    }
+    else
+        p += e;
+}
+
+Console.WriteLine(c);
