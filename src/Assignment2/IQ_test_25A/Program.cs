c27d5f8cff292d8e45cca06658cc48f308cb3d61
rehanmosabbir <rehanmosabbir@gmail.com>
2022-08-31 23:28:59 +0600

    solve IQ_test_25A

---
+++ src/Assignment2/IQ_test_25A/Program.cs
@@ -0,0 +1,40 @@
+﻿
+int a, j = 0, b = 0;
+a = int.Parse(Console.ReadLine());
+int[] arr = new int[a];
+string[] str = Console.ReadLine().Split(' ');
+for (int i = 0; i < a; i++)
+{
+    arr[i] = int.Parse(str[i]);
+    
+    if (arr[i] % 2 == 0)
+    {
+        j++;
+    }
+    else
+    {
+        b++;
+    }
+}
+if (j > b)
+{
+    for (int i = 0; i < a; i++)
+    {
+        if (arr[i] % 2 != 0)
+        {
+            Console.WriteLine(i+1);
+            break;
+        }
+    }
+}
+else
+{
+    for (int i = 0; i < a; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            Console.WriteLine(i+1);
+            break;
+        }
+    }
+}
