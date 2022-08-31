e265f4cbaab9db496382acb725354fe5357c342b
rehanmosabbir <rehanmosabbir@gmail.com>
2022-08-31 11:15:37 +0600

    solve Pangram_520A

---
+++ src/Assignment2/Pangram_520A/Program.cs
@@ -0,0 +1,22 @@
+﻿int n, i, j, cn = 0;
+n = int.Parse(Console.ReadLine());
+char[] b = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
+string str = Console.ReadLine();
+
+string a = str.ToUpper();
+
+for (i = 0; i < 26; i++)
+{
+    for (j = 0; j < n; j++)
+
+        if (b[i] == a[j])
+        {
+            cn++;
+            break;
+        }
+
+}
+
+if (cn == 26) Console.WriteLine("YES"); 
+else Console.WriteLine("NO");
