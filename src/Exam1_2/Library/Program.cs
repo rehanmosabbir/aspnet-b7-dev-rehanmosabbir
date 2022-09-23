c2951af7f5e5f7ab2fe736ee2360e63164b5d696
rehanmosabbir <rehanmosabbir@gmail.com>
2022-09-23 20:24:30 +0600

    create member and admin areas

--- src/Exam1_2/Library/Program.cs
+++ src/Exam1_2/Library/Program.cs
@@ -57,6 +57,10 @@ try
 
     app.UseAuthentication();
     app.UseAuthorization();
+    
+    app.MapControllerRoute(
+        name: "areas",
+        pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
     app.MapControllerRoute(
         name: "default",
