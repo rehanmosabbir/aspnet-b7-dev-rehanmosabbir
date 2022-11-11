c0691f3ee0158510a972b677dfc8afb0c259109f
rehanmosabbir <rehanmosabbir@gmail.com>
2022-11-11 23:56:17 +0600

    solve Division_1669A

---
+++ src/Assignment2/Division_1669A/Program.cs
@@ -0,0 +1,22 @@
+﻿int n = int.Parse(Console.ReadLine());
+int a;
+for(int i = 0; i < n; i++)
+{
+    a = int.Parse(Console.ReadLine());
+    if(a < 1400)
+    {
+        Console.WriteLine("Division 4");
+    }
+    else if(a >= 1400 && a < 1600)
+    {
+        Console.WriteLine("Division 3");
+    }
+    else if (a >= 1600 && a < 1900)
+    {
+        Console.WriteLine("Division 2");
+    }
+    else
+    {
+        Console.WriteLine("Division 1");
+    }
+}
