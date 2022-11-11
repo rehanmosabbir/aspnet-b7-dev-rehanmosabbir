ed92e46379f1539c3033f32fddfbde693649c95b
rehanmosabbir <rehanmosabbir@gmail.com>
2022-11-11 22:31:25 +0600

    solve Marathon_1692A

---
+++ src/Assignment2/Marathon_1692A/Program.cs
@@ -0,0 +1,27 @@
+﻿long t;
+t = int.Parse(Console.ReadLine());
+
+for(int i = 0; i < t; i++)
+{
+    long a, b, c, d;
+    int count = 0;
+    string[] line = Console.ReadLine().Split(" ");
+    a = int.Parse(line[0]);
+    b = int.Parse(line[1]);
+    c = int.Parse(line[2]);
+    d = int.Parse(line[3]);
+    if (b > a)
+    {
+        count++;
+    }
+    if (c > a)
+    {
+        count++;
+    }
+    if (d > a)
+    {
+        count++;
+    }
+    Console.WriteLine(count);
+
+}
