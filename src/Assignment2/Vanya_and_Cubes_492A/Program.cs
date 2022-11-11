37fb113e60c99a3c6b2787f06fc923d482f658ea
rehanmosabbir <rehanmosabbir@gmail.com>
2022-11-11 23:50:13 +0600

    solve Vanya_and_Cubes_492A

---
+++ src/Assignment2/Vanya_and_Cubes_492A/Program.cs
@@ -0,0 +1,15 @@
+﻿int a, i = 0, last = 0, current = 0;
+
+a = int.Parse(Console.ReadLine());
+while (a > 0)
+{
+    i++;
+    current = last + i;
+    last = current;
+    a -= current;
+    if (a < 0)
+    {
+        i--;
+    }
+}
+Console.WriteLine(i);
