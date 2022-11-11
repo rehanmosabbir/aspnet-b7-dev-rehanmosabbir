a408416d6ebf449ae7d0c92a4224786b921b6779
rehanmosabbir <rehanmosabbir@gmail.com>
2022-11-12 00:11:54 +0600

    solve Floor_Number_1426A

---
+++ src/Assignment2/Floor_Number_1426A/Program.cs
@@ -0,0 +1,15 @@
+﻿int t, n, x;
+t = int.Parse(Console.ReadLine());
+for(int i = 0; i < t; i++)
+{
+    string[] line = Console.ReadLine().Split(" ");
+    n = int.Parse(line[0]);
+    x = int.Parse(line[1]);
+    int c = 2, ans = 1;
+    while (c < n)
+    {
+        c += x;
+        ans++;
+    }
+    Console.WriteLine(ans);
+}
