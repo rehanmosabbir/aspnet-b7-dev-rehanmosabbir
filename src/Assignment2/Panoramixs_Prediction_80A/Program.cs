c58b3aa00e3ccf0ea5593f2f0db389a76c19bd59
rehanmosabbir <rehanmosabbir@gmail.com>
2022-11-11 22:18:21 +0600

    solve Panoramixs_Prediction_80A

---
+++ src/Assignment2/Panoramixs_Prediction_80A/Program.cs
@@ -0,0 +1,42 @@
+﻿int m, n;
+bool f = false;
+int[] a = new int[16]{
+        2,
+        3,
+        5,
+        7,
+        11,
+        13,
+        17,
+        19,
+        23,
+        29,
+        31,
+        37,
+        41,
+        43,
+        47,
+        0
+};
+
+string[] line = Console.ReadLine().Split(" ");
+n = int.Parse(line[0]);
+m = int.Parse(line[1]);
+for (int i = 0; i < 14; i++)
+{
+    if (a[i] == n)
+    {
+        if(a[i + 1] == m)
+        {
+            f = true;
+        }
+    }
+}
+if (f)
+{  
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
+}
