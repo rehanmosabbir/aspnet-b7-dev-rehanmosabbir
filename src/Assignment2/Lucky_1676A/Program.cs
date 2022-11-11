36ddc49fa1d4ff718ff02e3f71cd7f28639f445d
rehanmosabbir <rehanmosabbir@gmail.com>
2022-11-11 23:07:10 +0600

    solve Lucky_1676A

---
+++ src/Assignment2/Lucky_1676A/Program.cs
@@ -0,0 +1,23 @@
+﻿int t = int.Parse(Console.ReadLine());
+
+for(int i = 0; i < t; i++)
+{
+    string a = Console.ReadLine();
+    int j, sumf = 0, suml = 0;
+    for(j = 0; j < 3; j++)
+    {
+        sumf += (a[j] - '0');
+    }
+    for (j = 3; j < 6; j++)
+    {
+        suml += (a[j] - '0');
+    }
+    if(suml == sumf)
+    {
+        Console.WriteLine("YES");
+    }
+    else
+    {
+        Console.WriteLine("NO");
+    }
+}
