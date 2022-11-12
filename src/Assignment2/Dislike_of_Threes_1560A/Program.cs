34309e3224512707a9558220f6f08b4146b2b14a
rehanmosabbir <rehanmosabbir@gmail.com>
2022-11-12 13:52:05 +0600

    solve Dislike_of_Threes_1560A

---
+++ src/Assignment2/Dislike_of_Threes_1560A/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] arr = new int[1001];
+
+int j = 1;
+for (int i = 1; i <= 1666; i++)
+{
+    if (i % 3 != 0)
+    {
+        if (i % 10 != 3)
+        {
+            arr[j] = i;
+            j = j + 1;
+        }
+
+    }
+
+}
+
+int t;
+t = int.Parse(Console.ReadLine());
+int k;
+for(int l = 0; l < t; l++)
+{
+    k = int.Parse(Console.ReadLine());
+    Console.WriteLine(arr[k]);
+}
