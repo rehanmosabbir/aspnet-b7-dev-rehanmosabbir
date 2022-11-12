d5c7dbe97b17342bcc1aa3649e9cec90629b15f2
rehanmosabbir <rehanmosabbir@gmail.com>
2022-11-12 14:42:36 +0600

    solve Is_your_horseshoe_on_the_other_hoof_228A

---
+++ src/Assignment2/Is_your_horseshoe_on_the_other_hoof_228A/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] mas = new int[4]; 
+int e = 3;
+
+
+string[] line = Console.ReadLine().Split(" ");
+
+for (int i = 0; i < 4; i++)
+{
+    mas[i] = int.Parse(line[i]);
+}
+Array.Sort(mas);
+for (int i = 0; i < 3; i++)
+{
+    if (mas[i] != mas[i + 1])
+    {
+        e--;
+    }
+}
+Console.WriteLine(e);
