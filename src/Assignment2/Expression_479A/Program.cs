e9ddace240f2dd2dbd032fbc6a50e36c53a50f50
rehanmosabbir <rehanmosabbir@gmail.com>
2022-11-12 14:55:07 +0600

    solve Expression_479A

---
+++ src/Assignment2/Expression_479A/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] p = new int[6];
+
+
+int a, b, c, i;
+
+a = int.Parse(Console.ReadLine());
+b = int.Parse(Console.ReadLine());
+c = int.Parse(Console.ReadLine());
+
+p[0] = a * b + c;
+p[1] = a + b * c;
+p[2] = a * (b + c);
+p[3] = (a + b) * c;
+p[4] = a + b + c;
+p[5] = a * b * c;
+Array.Sort(p);
+Console.WriteLine(p[5]);
