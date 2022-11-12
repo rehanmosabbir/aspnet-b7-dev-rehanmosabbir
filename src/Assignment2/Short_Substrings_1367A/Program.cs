c292d8fe309d57fa3210415a14fbf0e647c487ff
rehanmosabbir <rehanmosabbir@gmail.com>
2022-11-12 13:32:52 +0600

    solve Short_Substrings_1367A

---
+++ src/Assignment2/Short_Substrings_1367A/Program.cs
@@ -0,0 +1,23 @@
+﻿
+int t;
+t = int.Parse(Console.ReadLine());
+for(int i = 0 ; i < t; i++)
+{
+    string str, ans = null;
+    str = Console.ReadLine();
+    int n = str.Length;
+    for(int j = 0; j < n; j++)
+    {
+        if(j == 0 || j == n - 1)
+        {
+            ans += str[j];
+        }
+        else
+        {
+            ans += str[j];
+            j++;
+        }
+    }
+    Console.WriteLine(ans);
+
+}
