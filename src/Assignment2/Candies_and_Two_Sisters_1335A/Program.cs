e01937d4bed31c373fea1e39d4bbc626d2951be7
rehanmosabbir <rehanmosabbir@gmail.com>
2022-11-12 14:13:59 +0600

    solve Candies_and_Two_Sisters_1335A

---
+++ src/Assignment2/Candies_and_Two_Sisters_1335A/Program.cs
@@ -0,0 +1,11 @@
+﻿int t;
+double n;
+long ans;
+t = int.Parse(Console.ReadLine());
+for(int i = 0; i < t; i++)
+{
+    ans = 0;
+    n = double.Parse(Console.ReadLine());
+    ans = (long)Math.Ceiling((n / 2) - 1);
+    Console.WriteLine(ans);
+}
