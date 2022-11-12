c36a713c4097bcc952c8379f5a0569e8152b5cae
rehanmosabbir <rehanmosabbir@gmail.com>
2022-11-12 13:57:08 +0600

    solve Required_Remainder_1374A

---
+++ src/Assignment2/Required_Remainder_1374A/Program.cs
@@ -0,0 +1,21 @@
+﻿long t;
+long x, y, n;
+t = long.Parse(Console.ReadLine());
+for(int i = 0; i < t; i++)
+{
+    string[] line = Console.ReadLine().Split(" ");
+    x = long.Parse(line[0]);
+    y = long.Parse(line[1]);
+    n = long.Parse(line[2]);
+    
+    long ans = 0;
+    ans = n - n % x + y;
+    if (ans <= n)
+    {
+        Console.WriteLine(ans);
+    }
+    else
+    {
+        Console.WriteLine((n - n % x - (x - y)));
+    }
+}
