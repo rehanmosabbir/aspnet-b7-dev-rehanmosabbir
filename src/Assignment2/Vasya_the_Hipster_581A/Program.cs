99ad965bee8dcdba38b802bba9f5628dfc02b02b
rehanmosabbir <rehanmosabbir@gmail.com>
2022-11-12 12:43:46 +0600

    solve Vasya_the_Hipster_581A

---
+++ src/Assignment2/Vasya_the_Hipster_581A/Program.cs
@@ -0,0 +1,23 @@
+﻿int a, b;
+string[] line = Console.ReadLine().Split(" ");
+a = int.Parse(line[0]);
+b = int.Parse(line[1]);
+
+if (a >= b)
+{
+    a -= b;
+    a /= 2;
+    Console.Write(b);
+    Console.Write(' ');
+    Console.Write(a);
+
+
+}
+else
+{
+    b -= a;
+    b /= 2;
+    Console.Write(a);
+    Console.Write(' ');
+    Console.Write(b);
+}
