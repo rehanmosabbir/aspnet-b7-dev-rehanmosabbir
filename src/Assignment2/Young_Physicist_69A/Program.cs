0d1bbc6785fc64458c24a3a667decdf439ceb6c3
rehanmosabbir <rehanmosabbir@gmail.com>
2022-11-12 14:27:13 +0600

    solve Young_Physicist_69A

---
+++ src/Assignment2/Young_Physicist_69A/Program.cs
@@ -0,0 +1,23 @@
+﻿int n, x, y, z, xsum = 0, ysum = 0, zsum = 0;
+n = int.Parse(Console.ReadLine());
+
+for(int i = 0; i < n; i++)
+{
+    string[] line = Console.ReadLine().Split(" ");
+    x = int.Parse(line[0]);
+    y = int.Parse(line[1]);
+    z = int.Parse(line[2]);
+    xsum += x;
+    ysum += y;
+    zsum += z;
+}
+
+if (xsum == 0 && ysum == 0 && zsum == 0)
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
+}
+
