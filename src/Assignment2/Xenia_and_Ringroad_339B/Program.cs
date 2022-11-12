ce2a68ba38992f849b869d4fb4aa631c74e5caa0
rehanmosabbir <rehanmosabbir@gmail.com>
2022-11-12 14:09:22 +0600

    solve Xenia_and_Ringroad_339B

---
+++ src/Assignment2/Xenia_and_Ringroad_339B/Program.cs
@@ -0,0 +1,28 @@
+﻿int n, m;
+string[] line = Console.ReadLine().Split(" ");
+
+n = int.Parse(line[0]);
+m = int.Parse(line[1]);
+
+int loc = 1;
+long ans = 0;
+
+int[] now = new int[m];
+string[] arr = Console.ReadLine().Split(" ");
+
+for (int i = 0; i < m; i++)
+{
+    now[i] = int.Parse(arr[i]);
+}
+for (int i = 0; i < m; i++)
+{
+    
+
+    if (now[i] >= loc)
+        ans += now[i] - loc;
+    else
+        ans += n - (loc - now[i]);
+    loc = now[i];
+}
+
+Console.WriteLine(ans);
