0ecba4cd52afb5c1db9e82a7a893c0a81a1c40ee
rehanmosabbir <rehanmosabbir@gmail.com>
2022-11-12 13:46:14 +0600

    solve Spy_Detected_1512A

---
+++ src/Assignment2/Spy_Detected_1512A/Program.cs
@@ -0,0 +1,33 @@
+﻿int t;
+t = int.Parse(Console.ReadLine());
+for(int i = 0; i < t; i++)
+{
+    int n;
+    n = int.Parse(Console.ReadLine());
+    int[] ar = new int[n];
+    string[] line = Console.ReadLine().Split(" ");
+    for (int j = 0; j < n; ++j)
+    {
+        ar[j] = int.Parse(line[j]);
+    }
+    bool found = false;
+    for (int k = 1; k < n - 1; ++k)
+    {
+        if (ar[k] != ar[k - 1] && ar[k] != ar[k + 1])
+        {
+            Console.WriteLine(k+1);
+            found = true;
+        }
+    }
+    if (!found)
+    {
+        if (ar[0] != ar[1])
+        {
+            Console.WriteLine(1);
+        }
+        else
+        {
+            Console.WriteLine(n);
+        }
+    }
+}
