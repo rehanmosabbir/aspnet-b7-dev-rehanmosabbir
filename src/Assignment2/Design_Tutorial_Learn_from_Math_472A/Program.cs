8dbe9050dfda9b7160a79665de384856fac4159f
rehanmosabbir <rehanmosabbir@gmail.com>
2022-11-12 13:08:08 +0600

    solve Design_Tutorial_Learn_from_Math_472A

---
+++ src/Assignment2/Design_Tutorial_Learn_from_Math_472A/Program.cs
@@ -0,0 +1,12 @@
+﻿int n;
+n = int.Parse(Console.ReadLine());
+if (n % 2 == 0)
+{
+    Console.Write(n - 4);
+    Console.Write(" 4");
+}
+else
+{
+    Console.Write(n - 9);
+    Console.Write(" 9");
+}
