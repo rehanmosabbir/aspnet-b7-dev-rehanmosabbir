cc001cca589a37ebb8460e0ca3221c3c16838a2c
rehanmosabbir <rehanmosabbir@gmail.com>
2022-11-13 13:11:00 +0600

    solve Combination_Lock_540A

---
+++ src/Assignment2/Combination_Lock_540A/Program.cs
@@ -0,0 +1,11 @@
+﻿int n;
+string a, b;
+n = int.Parse(Console.ReadLine());
+a = Console.ReadLine();
+b = Console.ReadLine();
+int ans = 0;
+
+for (int i = 0; i < n; i++)
+    ans += Math.Min(10 - Math.Abs(a[i] - b[i]), Math.Abs(a[i] - b[i]));
+
+Console.WriteLine(ans);
