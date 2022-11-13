1ad980ba33131bd7c16b95f227891e0b9172cce7
rehanmosabbir <rehanmosabbir@gmail.com>
2022-11-13 10:53:25 +0600

    solve Sum_1742A

---
+++ src/Assignment2/Sum_1742A/Program.cs
@@ -0,0 +1,18 @@
+﻿int t;
+t = int.Parse(Console.ReadLine());
+for (int i = 0; i < t; i++)
+{
+    int a, b, c;
+    string[] line = Console.ReadLine().Split(" ");
+    a = int.Parse(line[0]);
+    b = int.Parse(line[1]);
+    c = int.Parse(line[2]);
+    if(a == b + c || b == c + a || c == a + b)
+    {
+        Console.WriteLine("YES");
+    }
+    else
+    {
+        Console.WriteLine("NO");
+    }
+}
