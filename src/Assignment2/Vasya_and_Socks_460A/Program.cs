7320293f66e0a9d2a5ba1bacaa11478c6ec7d932
rehanmosabbir <rehanmosabbir@gmail.com>
2022-11-13 10:01:13 +0600

    solve Vasya_and_Socks_460A

---
+++ src/Assignment2/Vasya_and_Socks_460A/Program.cs
@@ -0,0 +1,13 @@
+﻿int a, b, div, mod;
+string[] line = Console.ReadLine().Split(" ");
+a = int.Parse(line[0]);
+b = int.Parse(line[1]);
+int sum = a;
+while (a >= b)
+{
+    div = (a / b);
+    sum = sum + div;
+    mod = (a % b);
+    a = (div + mod);
+}
+Console.WriteLine(sum);
