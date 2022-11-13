2f7a3e75e46e8e2fbb957b9db6743f0e0eddbb8a
rehanmosabbir <rehanmosabbir@gmail.com>
2022-11-13 11:17:34 +0600

    solve Fafa_and_his_Company_935A

---
+++ src/Assignment2/Fafa_and_his_Company_935A/Program.cs
@@ -0,0 +1,11 @@
+﻿int i, n, cnt = 1;
+n = int.Parse(Console.ReadLine());
+for (i = 2; i * i < n; i++)
+{
+    if (n % i == 0)
+        cnt += 2;
+}
+if ((double)Math.Sqrt(n) == (int)Math.Sqrt(n))
+    cnt++;
+
+Console.WriteLine(cnt);
