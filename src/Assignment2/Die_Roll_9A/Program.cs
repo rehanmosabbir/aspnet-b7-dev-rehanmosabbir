274f152359ba16c6aeefa5e1f4db831caf22d3b5
rehanmosabbir <rehanmosabbir@gmail.com>
2022-11-13 10:06:28 +0600

    solve Die_Roll_9A

---
+++ src/Assignment2/Die_Roll_9A/Program.cs
@@ -0,0 +1,8 @@
+﻿int Y, W;
+string[] line = Console.ReadLine().Split(" ");
+Y = int.Parse(line[0]);
+W = int.Parse(line[1]);
+
+string[] probability = new string[7]{ "", "1/1", "5/6", "2/3", "1/2", "1/3", "1/6" };
+int D = Math.Max(Y, W);
+Console.WriteLine(probability[D]);
