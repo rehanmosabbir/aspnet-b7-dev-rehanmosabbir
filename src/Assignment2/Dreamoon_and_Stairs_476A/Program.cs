b52ff63b7333593214ff4053db35e4a533caa160
rehanmosabbir <rehanmosabbir@gmail.com>
2022-11-13 10:58:42 +0600

    solve Dreamoon_and_Stairs_476A

---
+++ src/Assignment2/Dreamoon_and_Stairs_476A/Program.cs
@@ -0,0 +1,21 @@
+﻿int x, n, m;
+string[] line = Console.ReadLine().Split(" ");
+n = int.Parse(line[0]);
+m = int.Parse(line[1]);
+
+if (m > n)
+    x = -1;
+else
+{
+    if (n % 2 == 0)
+        x = n / 2;
+    else
+        x = (n / 2) + 1;
+    while (x % m != 0)
+    {
+        x++;
+    }
+}
+
+Console.WriteLine(x);
+
