253c907ff15b2a8aa95b6cb272e0dcd7959b2d7d
rehanmosabbir <rehanmosabbir@gmail.com>
2022-11-13 10:36:02 +0600

    solve Maximum_in _Table_509A

---
+++ src/Assignment2/Maximum_in _Table_509A/Program.cs
@@ -0,0 +1,23 @@
+﻿int a;
+int[,] arr = new int[10,10];
+
+a = int.Parse(Console.ReadLine());
+for (int i = 0; i < a; i++)
+{
+    for (int j = 0; j < a; j++)
+    {
+        if (i == 0)
+        {
+            arr[i,j] = 1;
+        }
+        else if (j == 0)
+        {
+            arr[i,j] = 1;
+        }
+        else
+        {
+            arr[i,j] = arr[i - 1,j] + arr[i,j - 1];
+        }
+    }
+}
+Console.WriteLine(arr[a - 1,a - 1]);
