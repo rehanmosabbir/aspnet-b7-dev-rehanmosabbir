cfa69f0c7d98fd390505af03e49df287a04a4efc
rehanmosabbir <rehanmosabbir@gmail.com>
2022-11-13 13:06:05 +0600

    solve File_Name_978B

---
+++ src/Assignment2/File_Name_978B/Program.cs
@@ -0,0 +1,13 @@
+﻿int len, i, k = 0;
+string str;
+len = int.Parse(Console.ReadLine());
+str = Console.ReadLine();
+
+
+for (i = 0; i < len - 2; i++)
+    if (str[i] == 'x' && str[i + 1] == 'x' && str[i + 2] == 'x')
+        ++k;
+
+    
+
+Console.WriteLine(k);
