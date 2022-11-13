f318542647ee271f5904451e6c032dd420d88ce8
rehanmosabbir <rehanmosabbir@gmail.com>
2022-11-13 10:47:01 +0600

    solve Keyboard_474A

---
+++ src/Assignment2/Keyboard_474A/Program.cs
@@ -0,0 +1,31 @@
+﻿string solve(string s1, char c)
+{
+    string s = "qwertyuiopasdfghjkl;zxcvbnm,./";
+    string answer=null;
+    int len = s1.Length;
+    for (int i = 0; i < len; i++)
+    {
+        int index = s.IndexOf(s1[i]);
+        if (c == 'R')
+        {
+            answer += s[index - 1];
+
+        }
+        else
+        {
+            answer += s[index + 1];
+        }
+
+    }
+    return answer;
+
+}
+
+
+string s2;
+char c;
+c = char.Parse(Console.ReadLine());
+s2 = Console.ReadLine();
+Console.WriteLine(solve(s2,c));
+
+
