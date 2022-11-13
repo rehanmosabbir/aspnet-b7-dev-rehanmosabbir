b8c539b2e928ef9e1aaaab0a4a62becfeefdec33
rehanmosabbir <rehanmosabbir@gmail.com>
2022-11-13 12:01:05 +0600

    solve Vus_the_Cossack_and_a_Contest_1186A

---
+++ src/Assignment2/Vus_the_Cossack_and_a_Contest_1186A/Program.cs
@@ -0,0 +1,14 @@
+﻿
+int n, m, k;
+string[] line = Console.ReadLine().Split(" ");
+n = int.Parse(line[0]);
+m = int.Parse(line[1]);
+k = int.Parse(line[2]);
+
+if (n > Math.Min(m, k))
+    Console.WriteLine("NO");
+else
+    Console.WriteLine("YES");
+
+
+
