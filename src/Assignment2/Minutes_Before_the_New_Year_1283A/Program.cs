b3e22458b809448de884c444b08d8fce493e8c27
rehanmosabbir <rehanmosabbir@gmail.com>
2022-11-13 10:16:22 +0600

    solve Minutes_Before_the_New_Year_1283A

---
+++ src/Assignment2/Minutes_Before_the_New_Year_1283A/Program.cs
@@ -0,0 +1,12 @@
+﻿int h, m, t, day, time;
+t = int.Parse(Console.ReadLine());
+for(int i = 0; i < t; i++)
+{
+    time = 0;
+    string[] line = Console.ReadLine().Split(" ");
+    h = int.Parse(line[0]);
+    m = int.Parse(line[1]);
+    day = 24 * 60;
+    time = h * 60 + m;
+    Console.WriteLine(day - time);
+}
