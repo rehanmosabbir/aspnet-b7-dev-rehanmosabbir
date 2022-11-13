2db3afbaaad730b848ce003698bb18d102b0d4f7
rehanmosabbir <rehanmosabbir@gmail.com>
2022-11-13 12:08:01 +0600

    solve Sum_of_Odd_Integers_1327A

---
+++ src/Assignment2/Sum_of_Odd_Integers_1327A/Program.cs
@@ -0,0 +1,17 @@
+﻿
+long i, j, k;
+long n, m, t;
+
+t = long.Parse(Console.ReadLine());
+for(i = 0; i < t; i++)
+{
+    string[] line = Console.ReadLine().Split(" ");
+    n = int.Parse(line[0]);
+    k = int.Parse(line[1]);
+    if (n % 2 == k % 2)
+    {
+        if (k * k > n) Console.WriteLine("NO");
+        else Console.WriteLine("YES");
+    }
+    else Console.WriteLine("NO");
+}
