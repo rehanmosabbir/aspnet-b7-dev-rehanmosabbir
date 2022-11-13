51a71157e80b91cb9de7f67ade453bfa4f1d7b91
rehanmosabbir <rehanmosabbir@gmail.com>
2022-11-13 11:13:20 +0600

    solve Spell_Check_1722A

---
+++ src/Assignment2/Spell_Check_1722A/Program.cs
@@ -0,0 +1,29 @@
+﻿int k;
+k = int.Parse(Console.ReadLine());
+for (int i = 0; i < k; i++)
+{
+    int n;
+    n = int.Parse(Console.ReadLine());
+    string s, t = "Timur";
+    s = Console.ReadLine();
+    
+
+    char[] s1 = s.ToCharArray();
+    Array.Sort(s1);
+    string s2 = new string(s1);
+
+
+    char[] t1 = t.ToCharArray();
+    Array.Sort(t1);
+    string t2 = new string(t1);
+
+    if (s2 == t2)
+    {
+        Console.WriteLine("YES");
+    }
+    else
+    {
+        Console.WriteLine("NO");
+    }
+
+}
