45afef906d2d2e68b8f64feae49f955909d05bc3
rehanmosabbir <rehanmosabbir@gmail.com>
2022-12-06 15:13:23 +0600

    install html agility pack and Scrape data from website

--- src/StockData/StockData.Worker/Worker.cs
+++ src/StockData/StockData.Worker/Worker.cs
@@ -1,3 +1,5 @@
+using HtmlAgilityPack;
+
 namespace StockData.Worker
 {
     public class Worker : BackgroundService
@@ -11,10 +13,47 @@ namespace StockData.Worker
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            List<string> item = new List<string>();
+            List<List<string>> items = new List<List<string>>();
+
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+                var url = "https://www.dse.com.bd/latest_share_price_scroll_l.php";
+                var web = new HtmlWeb();
+                var doc = web.Load(url);
+
+                //With XPath
+                string value;
+                var nodeNew = doc.DocumentNode.SelectSingleNode("//span[@class='" + "green" + "']");
+                value = nodeNew.InnerText;
+                // Console.WriteLine(value);
+
+
+                var node = doc.DocumentNode.SelectSingleNode("//div[contains(@class, 'table-responsive') and contains(@class, 'inner-scroll')]");
+                var table = node.SelectSingleNode("table");
+                var tbodies = table.SelectNodes("tr");
+
+                foreach (var tr in tbodies)
+                {
+                    foreach (var td in tr.SelectNodes("td"))
+                    {
+                        item.Add(td.InnerText.Trim());
+                        //Console.WriteLine(td.InnerText.Trim());
+                    }
+
+                    items.Add(item);
+                    item = new List<string>();
+                }
+
+                foreach(var i in items)
+                {
+                    foreach(var j in i)
+                    {
+                        Console.WriteLine(j);
+                    }
+                    Console.WriteLine("============");
+                }
+                await Task.Delay(30000, stoppingToken);
             }
         }
     }
