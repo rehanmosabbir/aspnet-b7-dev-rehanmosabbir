73dbe923dc72c4553f04404e9a5bd8dc99c35410
rehanmosabbir <rehanmosabbir@gmail.com>
2022-12-10 18:18:27 +0600

    change Status Value

--- src/StockData/StockData.Worker/Worker.cs
+++ src/StockData/StockData.Worker/Worker.cs
@@ -67,8 +67,13 @@ namespace StockData.Worker
                     }
                     Console.WriteLine();
                 }
+
+                if(value.ToLower() == "open")
+                {
+                    _stockService.CreateStockData(items);
+                }
                 
-                _stockService.CreateStockData(items);
+                
                 
                 await Task.Delay(60000, stoppingToken);
             }
